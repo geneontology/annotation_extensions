36b855f505645d72e3db9867914869fad3e9158b
David Osumi-Sutherland <dosumis@gmail.com>
2015-03-11 18:43:08 +0000

    basic script for pulling usage text from gorel and writing to files named for rel

--- src/generate_includes.py
+++ src/generate_includes.py
@@ -6,15 +6,20 @@ import json
 gorel = Brain()
 gorel.learn("http://purl.obolibrary.org/obo/go/extensions/gorel.owl")
 
-# Load json file containined name:ID dict of (or use YAML?) AEs
+# Declaring AE rels as list for now.
+relations = [ "GOREL_0001006" ]
+# Would be better to pull list of AE rels automatically from file.
+#With current structure, thus would require pulling subproperties.  Can't do that with Brain.
 
-# Iterate over list, pulling usage,
+# Iterate over list, pulling usage and saving to file named for relation:
 
-ID = "GOREL_1234567"
-usage = ""
+for r in relations:
+    label = gorel.getLabel(r)
+    usage = gorel.getAnnotation(r, "usage")
+    usage_md = open(label + "_usage.md", "w")
+    usage_md.write(usage)
+    usage_md.close()
 
-usage = gorel.getAnnotation(ID, usage)
-
-usage_md = open(usage + ".md", "w") 
+gorel.sleep()
 
 
