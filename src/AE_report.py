5a8ef42a2fc1aee3b9ce9530891936621ef7a684
David Osumi-Sutherland <dosumis@gmail.com>
2015-05-13 10:32:12 +0100

    usage reports now include source

--- src/AE_report.py
+++ src/AE_report.py
@@ -2,6 +2,11 @@ from com.ziclix.python.sql import zxJDBC
 from tsv2pdm import tab
 import re
 import sys
+#import mygene
+
+"""Generates usage report files for annotation extensions.  
+Optionally run with a '-t' argument for testing
+"""
 
 
 def __main__():
@@ -11,9 +16,12 @@ def __main__():
             tstat = 1
     con = get_con()
     report = get_all_manual_exptl_AE(con, tstat)
-    report.save_tab(path = "./", file_name = "AE_unrolled.tsv", sort_keys = ('ANNOTATION_EXTENSION',))
+    #report.headers.insert(object = 'GP_NAME', index = 1)
+    #for r in report.tab:
+    #    r['GP_NAME'] = find_gp_name(r['GP_ID'])
+    report.save_tab(path = "../includes/", file_name = "AE_unrolled.tsv", sort_keys = ('ANNOTATION_EXTENSION',))
     report = get_compact_AE(con)
-    report.save_tab(path = "./", file_name = "AE_compact_unrolled.tsv", sort_keys = ('ANNOTATION_EXTENSION',))
+    report.save_tab(path = "../includes/", file_name = "AE_compact_unrolled.tsv", sort_keys = ('ANNOTATION_EXTENSION',))
     con.close()
 
 def dict_cursor(cursor):
@@ -35,10 +43,10 @@ def get_con():
 def get_all_manual_exptl(con):
     cursor = con.cursor()
     cursor.execute("SELECT * FROM (" \
-                    "SELECT a.ENTITY_ID AS gp_id, a.GO_ID, t.name AS GO_name, et.NAME AS evidence, a.REF_DB_CODE AS ref_type, a.REF_DB_ID AS ref_acc, a.ANNOTATION_EXTENSION " \
+                    "SELECT a.ENTITY_ID AS gp_id, a.GO_ID, t.name AS GO_name, e2g AS evidence, a.REF_DB_CODE AS ref_type, a.REF_DB_ID AS ref_acc, a.ANNOTATION_EXTENSION " \
                     "from go.v_manual_annotations a " \
                     "join go.terms t on (t.go_id = a.go_id) " \
-                    "join go.eco_terms et on (a.ECO_ID = et.ECO_ID) " \
+                    "join GO.EVIDENCE2ECO e2g ON (e2g.eco_id = a.eco_id) " \
                     "where a.is_public = 'Y' " \
                     ") where ROWNUM <= 1000")
     return dict_cursor(cursor)
@@ -52,14 +60,16 @@ def get_all_manual_exptl_AE(con, tstat=0):
         pre = "SELECT * FROM ("
         post = ") WHERE ROWNUM <= 100"
     cursor = con.cursor()
-    query = "%s SELECT a.ENTITY_ID as GP_ID, a.GO_ID, t.name as GO_NAME, et.NAME as EVIDENCE, a.REF_DB_CODE AS REF_TYPE, a.REF_DB_ID AS REF_ACC, a.ANNOTATION_EXTENSION " \
+    query = "%s SELECT a.ENTITY_ID as GP_ID, a.GO_ID, t.name as GO_NAME, e2g.code as EVIDENCE, " \
+             "a.REF_DB_CODE AS REF_TYPE, a.REF_DB_ID AS REF_ACC, a.ANNOTATION_EXTENSION, a.SOURCE " \
                     "from go.v_manual_annotations a " \
                     "join go.terms t on (t.go_id = a.go_id) " \
-                    "join go.eco_terms et on (a.ECO_ID = et.ECO_ID) " \
+                    "join GO.EVIDENCE2ECO e2g ON (e2g.eco_id = a.eco_id) " \
                     "where a.is_public = 'Y' " \
                     "and a.annotation_extension is not null %s" % (pre, post)
     cursor.execute(query)
-    results_tab = tab(headers =  ['GP_ID', 'GO_ID', 'GO_NAME', 'EVIDENCE', 'REF_TYPE', 'REF_ACC', 'ANNOTATION_EXTENSION'])
+    # Specify headers in order to set order of columns for printing.
+    results_tab = tab(headers =  ['SOURCE', 'GP_ID', 'GO_ID', 'GO_NAME', 'ANNOTATION_EXTENSION', 'EVIDENCE', 'REF_TYPE', 'REF_ACC'])
     for r in dict_cursor(cursor):
         if r['ANNOTATION_EXTENSION']:
             results_tab.tab.extend(unroll_AE(r))
@@ -110,4 +120,16 @@ def shortName_map():
     ### Needs mapping to RO!.  Easiest way is probably to merge down GOREL + imports.  Then iterate over OPs (how?)
     return
 
+# def find_gp_name(ID):
+#     mg = mygene.MyGeneInfo()
+#     # Assuming uniprot for now...
+#     out = mg.query(ID, scopes='uniprot', fields='symbol')
+#     hits = out['hits']
+#     if hits:
+#         if len(hits) == 1:
+#             return hits[0]['symbol']
+#         else:
+#             return ''
+#     else:
+#         return ''
 __main__()
